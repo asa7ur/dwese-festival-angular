fbcf4334647d6264acbdc3123c12a3bd1580e601
Garik Asatryan <asa7ur@proton.me>
2026-03-02 11:45:33 +0100

    Improved Login logic

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import {AuthService} from '../../core/services/auth';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, AsyncPipe],
   templateUrl: './header.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private authService = inject(AuthService);
+
+  isLoggedIn$ = this.authService.isLoggedIn();
+  userName$ = this.authService.userName$;
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
